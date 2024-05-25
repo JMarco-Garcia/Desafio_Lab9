ee2156ce898fb54cd271dbc7664ecd2d91979367
Marco Garcia <118572129+JMarco-Garcia@users.noreply.github.com>
2024-05-25 13:30:07 -0500

    Add files via upload

---
+++ src/laboratorio09/Laboratorio09.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio09;
+
+import java.util.List;
+import java.util.ArrayList;
+import laboratorio09.Entidades.Estudiante;
+import laboratorio09.Pantallas.PantallaListaEstudiantes;
+import laboratorio09.Pantallas.PantallaDatosEstudiante;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Laboratorio09 {
+
+    public static List<Estudiante> listaEstudiantes = new ArrayList<>();
+    public static void main(String[] args) {
+        PantallaListaEstudiantes plEstudiante = new PantallaListaEstudiantes();
+        plEstudiante.setVisible(true);
+    }
+    
+}
